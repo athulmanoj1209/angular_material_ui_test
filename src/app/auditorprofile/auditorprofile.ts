824074848c17495790c8e483f702b82610393dc4
Athul Manoj <athul.manoj@crowe.ae>
2026-03-10 00:06:13 +0400

    added dynamic data the tree and implemented the auditor detail page

---
+++ src/app/auditorprofile/auditorprofile.ts
@@ -0,0 +1,63 @@
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { ActivatedRoute } from '@angular/router';
+import { ImageService } from '../image.service';
+import { catchError, map, Observable } from 'rxjs';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { createGlobalPositionStrategy } from '@angular/cdk/overlay';
+
+@Component({
+  selector: 'app-auditorprofile',
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    NgIf,
+    AsyncPipe
+],
+  templateUrl: './auditorprofile.html',
+  styleUrl: './auditorprofile.css',
+})
+export class Auditorprofile implements OnInit {
+
+  private activatedRoute = inject(ActivatedRoute);
+  private imageService = inject(ImageService);
+  auditorInfo$!: Observable<any>;
+
+  private employeeId = this.activatedRoute.snapshot.paramMap.get('id');
+
+  async ngOnInit() {
+
+    this.auditorInfo$ = this.imageService.getAuthorById(this.employeeId!)
+      .pipe(
+        (map((response: any) => { return response?.data })),
+        catchError((error: Error) => {
+          console.log("error in admin register", error.message);
+          throw error;
+        })
+      );
+      
+    console.log(this.auditorInfo$);
+  }
+
+
+
+  auditor = {
+    name: "Alice Johnson",
+    type: "Lead Auditor",
+    empId: "AU1001",
+    dept: "IT",
+    contact: "alice.johnson@company.com",
+    image: "https://kahedu.edu.in/n/wp-content/uploads/2023/10/Auditing-Excellence_-The-art-of-Assurance-in-B-Com-Professional-accounting-1024x683.jpg",
+    description: "Alice Johnson is a Lead IT Auditor with over 10 years of experience in cybersecurity assessments, system controls evaluation, and regulatory compliance. She specializes in enterprise risk management and leads complex audit engagements across technology platforms."
+  };
+
+  onSendRequest(email: string) {
+    console.log(email);
+    window.location.href = `mailto:${email}`;
+   }
+
+}
